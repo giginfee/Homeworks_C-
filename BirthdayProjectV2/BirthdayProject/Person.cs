a46c080574c8ee54b558f632f61db7a0ac56fc09
giginfee <olya.bernatski@gmail.com>
2024-03-13 22:20:17 +0200

    Homework 2

---
+++ BirthdayProjectV2/BirthdayProject/Person.cs
@@ -0,0 +1,60 @@
+﻿using BirthdayProject.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayProject.ViewModels
+{
+    class Person
+    {
+        private readonly string _name;
+        private readonly string _surname;
+        private readonly string _email;
+        private readonly DateTime _birthDate;
+        private int _age;
+        private bool _isAdult;
+        private string _westernSign;
+        private string _chineseSign;
+        private bool _isBirthday;
+        private DateContoller _dateContoller;
+        private ZodiacDetector _zodiacDetector;
+        public string Name => _name;
+        public string Surname => _surname;
+        public string Email => _email;
+        public DateTime BirthDate => _birthDate;
+
+       
+        public int Age => _age;
+        public bool IsAdult => _isAdult;
+        public string WesternSign => _westernSign;
+        public string ChineseSign => _chineseSign;
+        public bool IsBirthday => _isBirthday;
+
+
+
+        public Person(string name, string surname, string email, DateTime birthDate)
+        {
+            _name = name;
+            _surname = surname;
+            _email = email;
+            _birthDate = birthDate;
+
+            _dateContoller = new DateContoller((DateTime)birthDate);
+            _zodiacDetector = new ZodiacDetector((DateTime)birthDate);
+            _westernSign = _zodiacDetector.GetWesternZodiacSign();
+            _chineseSign = _zodiacDetector.GetChineseZodiacSign();
+             _age=_dateContoller.GetFullYears();
+            _isAdult = _age >= 18;
+            _isBirthday = _dateContoller.IsToday();
+           
+        }
+
+        public Person(string name, string surname, string email) : this( name,  surname,  email, DateTime.Now.Date)
+        {}
+
+        public Person(string name, string surname, DateTime birthDate) : this(name, surname, "", birthDate)
+        {}
+    }
+}
---
+++ BirthdayProjectV2/BirthdayProject/Tools/DateContoller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayProject.Tools
+{
+    internal class DateContoller
+    {
+
+        private readonly DateTime _date;
+
+        public DateContoller(DateTime date)
+        {
+            _date = date;
+        }
+
+        public int GetFullYears()
+        {
+            DateTime currentDate = DateTime.Now;
+            int years = currentDate.Year - _date.Year;
+
+            if (currentDate.Month < _date.Month || currentDate.Month == _date.Month && currentDate.Day < _date.Day)
+            {
+                years--;
+            }
+
+            return years;
+        }
+        public bool IsToday()
+        {
+            return _date.Date == DateTime.Now.Date;
+        }
+        public bool IsValidBirthDate()
+        {
+            int years = GetFullYears();
+            return years >= 0 && years <= 135;
+        }
+    }
+}
